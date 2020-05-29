efe914e7cefb85c3cda8d16f7b4f1cdfb7ea1996
CT <cdydevs@gmail.com>
2020-05-29 10:19:21 -0400

    Add Mobile first design for navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
-/ Toggle  between adding and removing the "responsive" class to topnav when the user clicks on the icon /
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
